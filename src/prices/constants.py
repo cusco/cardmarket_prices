5c3ad8dddc4b2c56b872ffd6eca8f62cd4c02f12
tiago <cusco@tretas.eu>
2024-11-08 13:14:25 +0000

    fix: legal sets

--- src/prices/constants.py
+++ src/prices/constants.py
@@ -17,10 +17,13 @@ LEGAL_STANDARD_SETS = [
     5599,  # Murders at Karlov Manor: Extras
     5647,  # Outlaws of Thunder Junction
     5662,  # Outlaws of Thunder Junction: Extras
+    5664,  # The Big Score | 2024-04-19
     5658,  # Bloomburrow
     5659,  # Bloomburrow: Extras
     5806,  # Duskmourn: House of Horror
     5807,  # Duskmourn: House of Horror: Extras
+    5852,  # Magic: The Gathering Foundations | 2024-11-15
+    5931,  # Magic: The Gathering Foundations: Extras | 2024-11-15
 ]
 
 LEGAL_PIONEER_SETS = [
@@ -32,127 +35,108 @@ LEGAL_PIONEER_SETS = [
     1469,  # Born of the Gods | 2014-02-07
     1481,  # Journey into Nyx | 2014-05-02
     1485,  # Magic 2015 | 2014-07-18
-    1588,  # Khans of Tarkir: Promos | 2014-09-20
     1495,  # Khans of Tarkir | 2014-09-26
-    1589,  # Fate Reforged: Promos | 2015-01-17
     1522,  # Fate Reforged | 2015-01-23
-    1602,  # Dragons Of Tarkir: Promos | 2015-03-21
+    1588,  # Khans of Tarkir: Promos | 2014-09-20
+    1589,  # Fate Reforged: Promos | 2015-01-17
     1601,  # Dragons of Tarkir | 2015-03-27
-    1655,  # Magic Origins: Promos | 2015-07-11
+    1602,  # Dragons Of Tarkir: Promos | 2015-03-21
     1652,  # Magic Origins | 2015-07-17
-    1670,  # Battle for Zendikar: Promos | 2015-09-26
+    1655,  # Magic Origins: Promos | 2015-07-11
     1668,  # Battle for Zendikar | 2015-10-02
-    1686,  # Oath of the Gatewatch: Promos | 2016-01-16
+    1670,  # Battle for Zendikar: Promos | 2015-09-26
     1676,  # Oath of the Gatewatch | 2016-01-22
-    1704,  # Shadows over Innistrad: Promos | 2016-04-02
+    1686,  # Oath of the Gatewatch: Promos | 2016-01-16
     1694,  # Shadows over Innistrad | 2016-04-08
-    1723,  # Eldritch Moon: Promos | 2016-07-16
     1695,  # Eldritch Moon | 2016-07-22
-    1726,  # Kaladesh: Promos | 2016-09-24
+    1704,  # Shadows over Innistrad: Promos | 2016-04-02
     1717,  # Kaladesh | 2016-09-30
+    1718,  # Aether Revolt | 2017-01-20
+    1723,  # Eldritch Moon: Promos | 2016-07-16
+    1726,  # Kaladesh: Promos | 2016-09-24
+    1729,  # Amonkhet | 2017-04-28
+    1731,  # Hour of Devastation | 2017-07-14
     1733,  # Kaladesh Inventions | 2016-09-30
     1750,  # Aether Revolt: Promos | 2017-01-14
-    1718,  # Aether Revolt | 2017-01-20
-    1803,  # Amonkhet: Promos | 2017-04-22
-    1729,  # Amonkhet | 2017-04-28
     1798,  # Amonkhet Invocations | 2017-04-28
-    1825,  # Hour of Devastation: Promos | 2017-07-08
-    1731,  # Hour of Devastation | 2017-07-14
-    1847,  # Ixalan: Promos | 2017-09-23
+    1803,  # Amonkhet: Promos | 2017-04-22
     1812,  # Ixalan | 2017-09-29
+    1822,  # Dominaria | 2018-04-27
+    1825,  # Hour of Devastation: Promos | 2017-07-08
+    1829,  # Rivals of Ixalan | 2018-01-19
     1830,  # Explorers of Ixalan | 2017-11-24
+    1835,  # Core 2019 | 2018-07-13
+    1847,  # Ixalan: Promos | 2017-09-23
     2023,  # Store Championship Promos | 2017-12-25
     2063,  # Rivals of Ixalan: Promos | 2018-01-13
-    1829,  # Rivals of Ixalan | 2018-01-19
     2315,  # Dominaria: Promos | 2018-04-21
-    1822,  # Dominaria | 2018-04-27
     2337,  # Core 2019: Promos | 2018-07-07
-    1835,  # Core 2019 | 2018-07-13
+    2348,  # Guilds of Ravnica | 2018-10-05
     2371,  # Guilds of Ravnica: Extras | 2018-09-29
     2373,  # Guilds of Ravnica: Mythic Edition | 2018-10-03
-    2348,  # Guilds of Ravnica | 2018-10-05
     2379,  # Guilds of Ravnica: Guild Kits | 2018-11-02
+    2411,  # Ravnica Allegiance | 2019-01-25
     2419,  # Ravnica Allegiance: Extras | 2019-01-19
     2422,  # Ravnica Allegiance: Mythic Edition | 2019-01-24
-    2411,  # Ravnica Allegiance | 2019-01-25
     2423,  # Ravnica Allegiance: Guild Kits | 2019-02-15
     2439,  # War of the Spark | 2019-04-27
-    2451,  # War of the Spark: Extras | 2019-04-27
-    2461,  # War of the Spark: Japanese Alternate-Art Planeswalkers | 2019-04-27
-    2459,  # War of the Spark: Mythic Edition | 2019-05-03
-    2490,  # Core 2020: Extras | 2019-06-29
     2447,  # Core 2020 | 2019-07-12
-    2694,  # Throne of Eldraine: Promos | 2019-09-27
+    2451,  # War of the Spark: Extras | 2019-04-27
+    2459,  # War of the Spark: Mythic Edition | 2019-05-03
+    2461,  # War of the Spark: Japanese Alternate-Art Planeswalkers | 2019-04-27
+    2490,  # Core 2020: Extras | 2019-06-29
     2582,  # Throne of Eldraine | 2019-10-04
     2587,  # Throne of Eldraine: Extras | 2019-10-04
-    2999,  # Theros Beyond Death: Promos | 2020-01-17
+    2694,  # Throne of Eldraine: Promos | 2019-09-27
     2814,  # Theros Beyond Death | 2020-01-24
-    2961,  # Theros Beyond Death: Extras | 2020-01-24
     2819,  # Ikoria: Lair of Behemoths | 2020-05-15
+    2961,  # Theros Beyond Death: Extras | 2020-01-24
+    2999,  # Theros Beyond Death: Promos | 2020-01-17
+    3048,  # Core 2021 | 2020-07-03
     3113,  # Ikoria: Lair of Behemoths: Extras | 2020-05-15
     3118,  # Ikoria: Lair of Behemoths: Promos | 2020-05-15
-    3048,  # Core 2021 | 2020-07-03
     3309,  # Core 2021: Extras | 2020-07-03
     3319,  # Core 2021: Promos | 2020-07-03
     3404,  # Zendikar Rising | 2020-09-25
     3474,  # Zendikar Rising: Extras | 2020-09-25
-    3500,  # Zendikar Rising: Promos | 2020-09-25
     3660,  # Kaldheim | 2021-02-05
     3680,  # Kaldheim: Extras | 2021-02-05
-    3723,  # Kaldheim: Promos | 2021-02-05
     3741,  # Strixhaven: School of Mages | 2021-04-23
     3761,  # Strixhaven: School of Mages: Extras | 2021-04-23
-    4176,  # Strixhaven: School of Mages: Promos | 2021-04-23
     4259,  # Adventures in the Forgotten Realms | 2021-07-23
     4260,  # Adventures in the Forgotten Realms: Extras | 2021-07-23
-    4358,  # Adventures in the Forgotten Realms: Promos | 2021-07-23
     4362,  # Innistrad: Midnight Hunt | 2021-09-24
     4363,  # Innistrad: Midnight Hunt: Extras | 2021-09-24
-    4383,  # Innistrad: Midnight Hunt: Promos | 2021-09-24
     4364,  # Innistrad: Crimson Vow | 2021-11-19
-    4423,  # Innistrad: Crimson Vow: Extras | 2021-11-19
-    4461,  # Innistrad: Crimson Vow: Promos | 2021-11-19
     4401,  # Innistrad: Double Feature | 2022-01-28
+    4423,  # Innistrad: Crimson Vow: Extras | 2021-11-19
     4472,  # Kamigawa: Neon Dynasty | 2022-02-18
     4473,  # Kamigawa: Neon Dynasty: Extras | 2022-02-18
-    4947,  # Kamigawa: Neon Dynasty: Promos | 2022-02-18
     4975,  # Streets of New Capenna | 2022-04-29
     4976,  # Streets of New Capenna: Extras | 2022-04-29
-    5039,  # Streets of New Capenna: Promos | 2022-04-29
     5072,  # Dominaria United | 2022-09-09
     5073,  # Dominaria United: Extras | 2022-09-09
-    5118,  # Dominaria United: Promos | 2022-09-09
     5164,  # The Brothers' War | 2022-11-18
     5165,  # The Brothers' War: Extras | 2022-11-18
-    5178,  # The Brothers' War: Promos | 2022-11-18
     5184,  # Phyrexia: All Will Be One | 2023-02-10
     5191,  # Phyrexia: All Will Be One: Extras | 2023-02-10
-    5197,  # Phyrexia: All Will Be One: Promos | 2023-02-10
+    5208,  # March of the Machine: The Aftermath: Extras | 2023-05-12
     5227,  # March of the Machine | 2023-04-21
     5278,  # March of the Machine: Extras | 2023-04-21
-    5310,  # March of the Machine: Promos | 2023-04-21
     5320,  # March of the Machine: The Aftermath | 2023-05-12
-    5208,  # March of the Machine: The Aftermath: Extras | 2023-05-12
     5359,  # Wilds of Eldraine | 2023-09-08
     5428,  # Wilds of Eldraine: Extras | 2023-09-08
-    5463,  # Wilds of Eldraine: Promos | 2023-09-08
-    5523,  # Special Guests | 2023-11-17
     5490,  # The Lost Caverns of Ixalan | 2023-11-17
     5491,  # The Lost Caverns of Ixalan: Extras | 2023-11-17
-    5521,  # The Lost Caverns of Ixalan: Promos | 2023-11-17
     5561,  # Murders at Karlov Manor | 2024-02-09
     5599,  # Murders at Karlov Manor: Extras | 2024-02-09
-    5600,  # Murders at Karlov Manor: Promos | 2024-02-09
-    5665,  # Breaking News | 2024-04-19
     5647,  # Outlaws of Thunder Junction | 2024-04-19
-    5662,  # Outlaws of Thunder Junction: Extras | 2024-04-19
-    5669,  # Outlaws of Thunder Junction: Promos | 2024-04-19
-    5664,  # The Big Score | 2024-04-19
     5658,  # Bloomburrow | 2024-08-02
     5659,  # Bloomburrow: Extras | 2024-08-02
-    5789,  # Bloomburrow: Promos | 2024-08-02
+    5662,  # Outlaws of Thunder Junction: Extras | 2024-04-19
+    5664,  # The Big Score | 2024-04-19
     5806,  # Duskmourn: House of Horror | 2024-09-27
     5807,  # Duskmourn: House of Horror: Extras | 2024-09-27
-    5885,  # Duskmourn: House of Horror: Promos | 2024-09-27
     5852,  # Magic: The Gathering Foundations | 2024-11-15
+    5931,  # Magic: The Gathering Foundations: Extras | 2024-11-15
 ]
